c3eb8fac26b860450415bbf452b8e6ff081433b5
LorsenLamour <sirvalentino0@gmail.com>
2025-11-04 09:46:29 -0500

    Added login and singUp page

---
+++ src/app/forms/signUpPage.jsx
@@ -0,0 +1,12 @@
+import { Text,View } from "react-native";
+
+export default function SignUpPage(){
+    return(
+        <View>
+            <Text>Please Sign Up to have acces to more functionality</Text>
+        
+        
+        </View>
+    )
+
+}
