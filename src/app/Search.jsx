3d105935ab0a11a2985f25329169a4c1bc8a9b36
BradLeneus <150571637+BradLeneus@users.noreply.github.com>
2025-11-23 19:28:45 -0500

    Search bar is finished.Will work on the modal for the search page

--- src/app/Search.jsx
+++ src/app/Search.jsx
@@ -1,12 +1,69 @@
-import { Tex,View } from "react-native";
-import SearchbarComponent, { SearchBarComponent } from "../components/SearchComponent"
+import React, { useContext } from 'react';
+import { View ,TouchableOpacity,Image} from 'react-native';
+import SearchbarComponent from '../components/SearchComponent';
+import { useThemedStyle, ThemedStyle } from '../hook/useThemedStyle';
+import dark from '../style/themes/dark.ts';
+import light from '../style/themes/light.ts';
+import { CS } from '../style/CommonStyle';
 
-export default function Search(){
-    return(
-        <View>
-            <SearchbarComponent/>        
-        
+const stylesFactory = ThemedStyle((theme,label) => ({
+    container: {
+        flex: 1,
+        backgroundColor: label === 'dark'? dark.background: light.background,
+        ...CS.Flex.column(24),
+        ...CS.padding(24),
+    },
+    headerRow: {
+        ...CS.Flex.row(2),
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    searchbarContainer: {
+        flex: 1,
+        marginRight: 10,
+    },
+    searchbar: {
+        backgroundColor: label === 'dark'? dark.backgroundPanel:light.backgroundPanel,
+        borderRadius: 24,
+        ...CS.padding(2),
+    },
+    searchbarInput: {
+        color: label === 'dark'?dark.text:light.text
+    },
+    
+    
+    
+    settingIconColor: label === 'dark' ? dark.primary : light.primary,
+    
+    settingIcon: label === 'dark' ? require('../icons/search_settings_icon_dark.png') : require('../icons/search_settings_icon.png'),
+
+    searchbarIconColor: label === 'dark'?dark.primary:'grey',
+    searchbarPlaceHolder:  label === 'dark'?dark.text:light.text,
+
+}));
+
+export default function Search() {
+    const searchBarStyles = useThemedStyle(stylesFactory);
+    
+
+    return (
+        <View style={searchBarStyles.container}>
+            <View style={searchBarStyles.headerRow}>
+                <View style={searchBarStyles.searchbarContainer}>
+                    <SearchbarComponent
+                        style={searchBarStyles.searchbar}
+                        inputStyle={searchBarStyles.searchbarInput}
+                        iconColor={searchBarStyles.searchbarIconColor}
+                        placeHolderColor={searchBarStyles.searchbarPlaceHolder}
+                    />
+                </View>
+                <TouchableOpacity>
+                    <Image 
+                        source={searchBarStyles.settingIcon}
+                        style={{ width: 24, height: 24 }}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
-    )
-
+    );
 }
--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,12 +1,18 @@
 import * as React from 'react';
 import { Searchbar } from 'react-native-paper';
 
-const SearchbarComponent = () => {
+const SearchbarComponent = ({ style, inputStyle, iconColor,placeHolderColor }) => {
   const [searchQuery, setSearchQuery] = React.useState('');
+  
+ 
 
   return (
     <Searchbar
+      style={style}
+      inputStyle={ inputStyle}
+      iconColor={iconColor}
       placeholder="Search currency :"
+      placeholderTextColor={placeHolderColor}
       onChangeText={setSearchQuery}
       value={searchQuery}
     />
