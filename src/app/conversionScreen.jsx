3bb423006f1512a2c516d20b3eff00906388066c
WilliamGirouard <santco123@hotmail.fr>
2025-11-29 00:52:13 -0500

    Ajout du call de l'API pour get le rate et changement du fonctionnement de la fonction du calcul de conversion. It works ! Havent tried gold or silver tho

--- src/app/conversionScreen.jsx
+++ src/app/conversionScreen.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { View, Text, StyleSheet, TextInput, TouchableOpacity, FlatList, Image } from 'react-native';
 import { Picker } from "@react-native-picker/picker"
 import { convertingCurrency, getConversionRate } from '../components/conversionComponents';
@@ -7,11 +7,12 @@ import  { flagsMap } from "../data/flags";
 
 //Source picker : https://github.com/react-native-picker/picker
 
-export default function pageConversion() {
+export default function PageConversion() {
     const [ baseCurrency, setBaseCurrency ] = useState("CAD");
     const [ convertToCurrency, setConvertToCurrency] = useState("USD");
     const [ inputValue, setInputValue] = useState(0);
     const [ convertedValue, setConvertedValue] = useState(0);
+    const rate = getConversionRate(baseCurrency, convertToCurrency);
 
     return (
         <View> 
@@ -39,11 +40,11 @@ export default function pageConversion() {
                 <View>
                     <View>
                         <TextInput placeholder='Insert the amount to convert' inputMode='decimal' maxLength={10} onChangeText={setInputValue}></TextInput>
-                        <TouchableOpacity onPress={() => convertingCurrency(baseCurrency, convertToCurrency, inputValue)}>
+                        <TouchableOpacity onPress={() => setConvertedValue(convertingCurrency(rate, inputValue))}>
                         <Text>Enter</Text>
                     </TouchableOpacity>
                 </View>
-                    <Text>Rate : {getConversionRate(baseCurrency, convertToCurrency)}</Text>
+                    <Text>Rate : {rate}</Text>
                     <Text>{convertedValue}</Text>
                 </View>
             </View>
--- src/components/conversionComponents.jsx
+++ src/components/conversionComponents.jsx
@@ -1,23 +1,26 @@
 import React, { useState, useEffect } from 'react';
-import { axios } from "axios";
-const getConversionRate = (defaultCurrency, changeToCurrency) => {
-    const [conversionRate, setConversionRate] = useState([]);
-        const fetchData = async () => {
-          try {
-            const response = await axios.get(`https://api.fxratesapi.com/latest?base=${defaultCurrency}&currencies=${changeToCurrency}&resolution=1m&format=json`);
-            setConversionRate(response.data.rates);
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        };
-        fetchData();
-    return conversionRate
+import axios from "axios";
+
+export function getConversionRate (baseCurrency, convertToCurrency)  {
+    const [conversionRate, setConversionRate] = useState(0);
+    
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await axios.get(`https://api.fxratesapi.com/latest?base=${baseCurrency}&currencies=${convertToCurrency}&resolution=1m&format=json`);
+          setConversionRate(response.data.rates[convertToCurrency]);
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      };
+    fetchData();
+    }, [baseCurrency, convertToCurrency]);
+  return conversionRate
 }
 
-const convertingCurrency = (defaultCurrency, changeToCurrency, value) => {
-    let conversionRate = getConversionRate(defaultCurrency, changeToCurrency)
-    let convertedValue =  (value * conversionRate)
-    return convertedValue
-}
-
-export { convertingCurrency, getConversionRate }
+export function convertingCurrency(rate, value) {
+  if (!rate) {
+    return 0;
+  }
+  return (value * rate)
+};
