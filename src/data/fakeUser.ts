bca15f352103ba24a3cc91418cd27424fdf77137
LorsenLamour <sirvalentino0@gmail.com>
2025-12-20 11:17:51 -0500

    created a fakeUser and return error message if the login failed

---
+++ src/data/fakeUser.ts
@@ -0,0 +1,6 @@
+export const fakeUser = {
+    id: "1",
+    email: "test@test.com",
+    password: "123456",
+    name: "Test User"
+};
--- src/hook/useUser.ts
+++ src/hook/useUser.ts
@@ -4,15 +4,17 @@ import { useRouter } from "expo-router";
 
 export const useUser = () => {
     return useContext(UserContext);
-}
+};
 
-export const assertUser = (path = "/login") => {
-    const user   = useUser();
+export const useAssertUser = (path = "/login") => {
+    const user = useUser();
     const router = useRouter();
+
     useLayoutEffect(() => {
         if (!user.isLogged) {
             router.replace(path);
         }
     }, [user.isLogged]);
+
     return user.isLogged;
-}
+};
